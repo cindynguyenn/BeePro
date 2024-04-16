805792f3bec4c8451bcd7fce4ba5e62139e3be4c
cindynguyenn <107452053+cindynguyenn@users.noreply.github.com>
2024-04-16 15:54:26 -0700

    Update BeePro4.0.py

--- BeePro4.0.py
+++ BeePro4.0.py
@@ -15,6 +15,7 @@ from dropbox.files import WriteMode
 # ADD OPTION TO RECORD_START FOR START AND END TIME
 # FIGURE OUT START AND END DATE PORTION OF CODE
 # maybe fix r/u to drpobox and google drive. make separate functions so error does not show up while we are waiting on wifi?
+# FIX THE NAMING PATH!!
 
 recording = False
 
